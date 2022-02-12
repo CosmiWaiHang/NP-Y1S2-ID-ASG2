f60482bbbe88f1c5b5ceb4586c504f31e7733752
CossetCosmi <53483420+CossetCosmi@users.noreply.github.com>
2022-02-13 03:08:34 +0800

    Create Account.js component to handle all the api request (with RestDB).

---
+++ js/repository/account.js
@@ -0,0 +1,93 @@
+/*jshint esversion: 9 */
+
+const API = '620020386a79155501021871';
+
+
+const get = {
+    /**
+     * <b> Get the account by username from RestDB </b> <br />
+     *
+     * If {@param callback} function is provided,
+     *     - the callback function will be run upon receiving a response
+     *     - the request will run ASYNC mode
+     *     - a NULL will be return <br />
+     * If {@param callback} function is NOT provided,
+     *     - the request will run in SYNC mode
+     *     - the response will be return upon receiving <br />
+     *
+     * @param username username of the account
+     * @param callback optional, callback function
+     * @returns {null | result}
+     */
+    by_username: (username, callback = null) => {
+        let result = null;
+
+        const query = {
+            'username': username,
+        };
+
+        const settings = {
+            'async': !!callback,
+            'crossDomain': true,
+            'url': `https://npy1s2idasg2-e59d.restdb.io/rest/account?q=${JSON.stringify(query)}`,
+            'method': 'GET',
+            'headers': {
+                'content-type': 'application/json',
+                'x-apikey': API,
+                'cache-control': 'no-cache',
+            },
+        };
+
+        $.ajax(settings)
+         .done(response => !!callback ? callback(response) : result = response);
+
+        return result;
+    },
+};
+
+
+const post = {
+    /**
+     * <b> Send the data to RestDB </b> <br />
+     *
+     * If {@param callback} function is provided,
+     *     - the callback function will be run upon receiving a response
+     *     - the request will run in ASYNC mode
+     *     - a NULL will be return <br />
+     * If {@param callback} function is NOT provided,
+     *     - the request will run in SYNC mode
+     *     - the response will be return upon receiving <br />
+     *
+     * @param username username of the new account
+     * @param password hashed password of the new account
+     * @param callback optional, callback function
+     * @returns {null | result}
+     */
+    account: (username, password, callback = null) => {
+        let result = null;
+
+        const data = {
+            'username': username,
+            'password': password,
+        };
+
+        const settings = {
+            'async': !!callback,
+            'crossDomain': true,
+            'url': 'https://npy1s2idasg2-e59d.restdb.io/rest/account',
+            'method': 'POST',
+            'headers': {
+                'content-type': 'application/json',
+                'x-apikey': API,
+                'cache-control': 'no-cache',
+            },
+            'processData': false,
+            'data': JSON.stringify(data),
+        };
+
+        $.ajax(settings)
+         .done(response => !!callback ? callback(response) : result = response);
+
+        return result;
+    },
+};
