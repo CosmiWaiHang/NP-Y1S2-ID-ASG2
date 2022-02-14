4b72eac2785157cef50174ca186e007bd073a803
CossetCosmi <53483420+CossetCosmi@users.noreply.github.com>
2022-02-14 13:49:41 +0800

    Add request from the API AoT server

---
+++ js/component/aot.js
@@ -0,0 +1,150 @@
+/*jshint esversion: 9 */
+
+
+class AttackOnTitan {
+    constructor(id, name, image, about, rating, height, type, inheritorList, abilityList, aliasList) {
+        this._id = id;
+        this._name = name;
+        this._image = image;
+        this._about = about;
+        this._rating = rating;
+        this._height = height;
+        this._type = type;
+        this._inheritorList = inheritorList;
+        this._abilityList = abilityList;
+        this._aliasList = aliasList;
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    set id(value) {
+        this._id = value;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(value) {
+        this._name = value;
+    }
+
+    get image() {
+        return this._image;
+    }
+
+    set image(value) {
+        this._image = value;
+    }
+
+    get about() {
+        return this._about;
+    }
+
+    set about(value) {
+        this._about = value;
+    }
+
+    get rating() {
+        return this._rating;
+    }
+
+    set rating(value) {
+        this._rating = value;
+    }
+
+    get height() {
+        return this._height;
+    }
+
+    set height(value) {
+        this._height = value;
+    }
+
+    get type() {
+        return this._type;
+    }
+
+    set type(value) {
+        this._type = value;
+    }
+
+    get inheritorList() {
+        return this._inheritorList;
+    }
+
+    set inheritorList(value) {
+        this._inheritorList = value;
+    }
+
+    get abilityList() {
+        return this._abilityList;
+    }
+
+    set abilityList(value) {
+        this._abilityList = value;
+    }
+
+    get aliasList() {
+        return this._aliasList;
+    }
+
+    set aliasList(value) {
+        this._aliasList = value;
+    }
+}
+
+
+const aotRepo = {
+    convert: content => {
+        const json = JSON.parse(content);
+        const titanList = json['titans'];
+
+        const aotList = [];
+
+        for (let i = 0, max = titanList.length; i < max; i++) {
+            const titan = titanList[i];
+            const id = titan['id'];
+            const name = titan['name'];
+            const image = `https://attack-on-titan-server.herokuapp.com${titan['image']}`;
+            const about = titan ['about'];
+            const rating = titan ['rating'];
+            const height = titan ['height'];
+            const type = titan ['type'];
+            const inheritorList = titan['inheritors'];
+            const abilityList = titan['abilities'];
+            const aliasList = titan['otherNames'];
+
+            const aot = new AttackOnTitan(id, name, image, about, rating, height, type, inheritorList, abilityList, aliasList);
+
+            aotList.push(aot);
+        }
+
+        return aotList;
+    },
+
+    get: {
+        all: (limit = 10, onSuccess = null, onFailure = null) => {
+            let res = null;
+            let err = null;
+
+            const proxy = 'https://api.allorigins.win';
+            const source = encodeURIComponent(`https://attack-on-titan-server.herokuapp.com/titans?limit=${limit}`);
+
+            $.getJSON(`${proxy}/get?url=${source}`)
+             .done(response => {
+                 const aotList = aotRepo.convert(response.contents);
+
+                 !!onSuccess
+                     ? onSuccess(aotList)
+                     : res = aotList;
+             })
+             .fail((xhr, status, message) =>
+                 !!onFailure
+                     ? onFailure(xhr, status, message)
+                     : err = {xhr, status, message});
+        },
+    },
+};
