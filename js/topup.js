bf3632b1a1fcef67c4b350b72a1de932f4109c32
CossetCosmi <53483420+CossetCosmi@users.noreply.github.com>
2022-02-17 04:36:30 +0800

    Update topup.js

---
+++ js/topup.js
@@ -0,0 +1,30 @@
+/*jshint esversion: 9 */
+
+
+$(window).on('load', () => 'use stict');
+
+
+(() => {
+    const getUser = userId => {
+        const user = new User(userId);
+
+        return userRepo.get.by_id(user).res;
+    };
+
+    $('#btn-pay')
+        .click(() => {
+            const eSlider = $('#range-slider');
+            const amount = parseInt(eSlider.val());
+
+            const userId = sessionStorage.getItem('userId');
+            const user = getUser(userId);
+
+            user.balance += amount;
+
+            userRepo.put(user, user => {
+                $('#range-slider-hint')[0]
+                    .innerText = `Add balance successfully, you have SGD ${user.balance} in your account.`;
+
+            }, e => console.log(e));
+        });
+})();
