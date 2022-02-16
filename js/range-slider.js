989cef7582bf30008b1fb5e5a8cf8c8029a83a16
CossetCosmi <53483420+CossetCosmi@users.noreply.github.com>
2022-02-17 02:05:44 +0800

    Update range-slider.js

--- js/range-slider.js
+++ js/range-slider.js
@@ -2,12 +2,36 @@
 
 
 $(window).on('load', () => {
-    const slider = $('#range-slider');
-    const outputEl = $('.range-slider-value')[0];
+    const eSlider = $('#range-slider');
+    const eDisplay = $('.range-slider-value')[0];
 
     const separator = number => Number(number).toLocaleString('en-US');
 
-    slider.on('input', function () {
-        outputEl.innerText = `SGD ${separator(this.value)}`;
+    const max = parseInt(eSlider.attr('max'));
+
+    eSlider.on('input change', function () {
+        const value = parseInt(this.value);
+
+        if (value < max) {
+            $('#range-slider-hint')[0].innerText = 'Please give me your angbao money ;)';
+        }
+
+        eDisplay.innerText = `SGD ${separator(this.value)}`;
     });
+
+    $('.btn-cute.cute')
+        .each((index, element) => {
+            const button = $(element);
+
+            button.click(() => {
+                const value = button.attr('value');
+                const amount = parseInt(eSlider.val()) + parseInt(value);
+
+                if (max < amount) {
+                    $('#range-slider-hint')[0].innerText = 'Meow, my wallet is too small to fit so much money at once :(';
+                }
+
+                eSlider.val(amount).change();
+            });
+        });
 });
